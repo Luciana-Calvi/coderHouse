6cd4d9d23984ed96e8980207a4ea2b49ff001145
Luci <lucalvi64@gmail.com>
2022-07-07 21:45:16 -0300

    clase 1

---
+++ Clase 1/ejercicio1.js
@@ -0,0 +1,16 @@
+let nombre = prompt("Ingrese su nombre");
+let apellido = prompt("Ingrese su apellido");
+
+let mensaje = "Hola"+" "+nombre + " " + apellido;
+console.log(mensaje); 
+
+let numero = prompt("Ingrese un numero");
+let num =10;
+let total=Number(numero) + num;
+console.log("El resultado es:"+" "+total);
+
+let usuario = prompt("Ingrese un nombre de usuario");
+let portada = prompt ("Ingrese un mensaje de saludo");
+let bienvenida = "Hola"+" "+"yo soy"+" "+ usuario+" "+portada;
+alert(bienvenida);
+
